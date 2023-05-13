1538c8f514d6d19486cfbcf47d64b94f16a9ac91
meshkat mohammadi <mahdieh.mohammadi1997@gmail.com>
2023-05-13 22:29:09 +0430

    adding weather icon

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// 1
+// 1a
 function showDate(dateIndex) {
   let days = [
     "sunday",
@@ -25,13 +25,17 @@ let time = hours + ":" + minutes;
 let showTime = document.querySelector("#show-time");
 showTime.innerHTML = dayName + " " + time;
 // 2
+
+function search(city){
+let apiKey = "1a6432c5ca7b6f9b0bee45c98d54ea71";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(showCurrentTemp);
+}
+
 function displayName(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input");
-
-  let apiKey = "1a6432c5ca7b6f9b0bee45c98d54ea71";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showCurrentTemp);
+  search(cityInput.value);
 }
 function showCurrentTemp(response) {
   let city = document.querySelector("#city-name");
@@ -39,12 +43,18 @@ function showCurrentTemp(response) {
   let temp = Math.round(response.data.main.temp);
   let degree = document.querySelector("#degree");
   degree.innerHTML = temp;
-  let desc = document.querySelector("small");
+  let desc = document.querySelector("#description");
   desc.innerHTML = response.data.weather[0].description;
   let windShow = document.querySelector("#wind");
   windShow.innerHTML = response.data.wind.speed;
   let humidityShow = document.querySelector("#humidity");
+  let iconElement = document.querySelector("#icon");
+
   humidityShow.innerHTML = response.data.main.humidity;
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 function handlePosition(position) {
@@ -64,3 +74,4 @@ searchFrom.addEventListener("submit", displayName);
 
 let getCurrent = document.querySelector("#current-location");
 getCurrent.addEventListener("click", getCurrentTemp);
+search("New York");
